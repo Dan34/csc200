81c7a312293db6978c0b2e2edcbbf884c06e87e7
der2932 <der2932@AN112495-CT103.nvstu.edu>
2015-10-08 17:54:36 -0400

    push

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+import javax.lang.model.element.VariableElement;
+
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println((6826589%10)+1);
+		System.out.print(1%2);
+	}
+}
---
+++ HelloWorld/src/test2.java
@@ -0,0 +1,8 @@
+
+public class test2 {
+	public static void main(String[]args) {
+		System.out.println("print");
+		String StringName ="name";
+		System.out.print(StringName+".");
+	}
+}
